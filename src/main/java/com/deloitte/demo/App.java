e30f4c93a377242b2a4e9af641b137a8edb4eeb2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-09-19 16:39:33 +0530

    jpa demo

--- src/main/java/com/deloitte/demo/App.java
+++ src/main/java/com/deloitte/demo/App.java
@@ -1,13 +1,10 @@
 package com.deloitte.demo;
 
 /**
- * Hello world!
- *
+ * @author Vaman Deshmukh 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
